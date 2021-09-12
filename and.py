a77ac325bd570590c9a190d82925ff5aef12c8ca
omkarsingh1008 <omkarsinghrajpurohit363@gmail.com>
2021-09-12 21:38:06 +0530

    create entrypoint

--- and.py
+++ and.py
@@ -3,26 +3,32 @@ from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
 import numpy as np
 
+def main(df,ETA,EPOCHS):
+    
 
-AND = {
+    df = pd.DataFrame(df)
+
+    print(df)
+
+    X,y = prepare_data(df)
+
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+
+    model = Perceptron(eta=ETA, epochs=EPOCHS)
+    model.fit(X, y)
+
+    _ = model.total_loss()
+
+    save_model(model, filename="and.model")
+    save_plot(df, "and.png", model)
+
+if __name__ == "__main__":
+    AND = {
     "x1": [0,0,1,1],
     "x2": [0,1,0,1],
     "y": [0,0,0,1],
-}
-
-df = pd.DataFrame(AND)
-
-print(df)
-
-X,y = prepare_data(df)
-
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
-
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
-
-_ = model.total_loss()
-
-save_model(model, filename="and.model")
-save_plot(df, "and.png", model)
+}   
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+    main(AND,ETA,EPOCHS)
